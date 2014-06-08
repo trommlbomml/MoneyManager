f8a5365c6524ff85428822a815a0d5b950368b2f
trommlbomml <peter_friedland@gmx.de>
2014-06-08 11:05:15 +0200

    Added Projects for Model implementation. NuGet-Packages for Test Project.

---
+++ MoneyManagerApplication/MoneyManager.Interfaces/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Interfaces
+{
+    public interface Repository
+    {
+        void Save();
+
+        void Load(string fileName);
+    }
+}
---
+++ MoneyManagerApplication/MoneyManager.Model/RepositoryImp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MoneyManager.Interfaces;
+
+namespace MoneyManager.Model
+{
+    class RepositoryImp : Repository
+    {
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
