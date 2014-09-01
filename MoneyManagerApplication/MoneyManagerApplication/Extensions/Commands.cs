2d82da80aab7f69fc6883a8c09d1a4d8872c027b
trommlbomml <peter_friedland@gmx.de>
2014-09-01 18:36:30 +0200

    Added DoubleClick on Request Entry to edit.

---
+++ MoneyManagerApplication/MoneyManagerApplication/Extensions/Commands.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MoneyManagerApplication.Extensions
+{
+    internal class Commands
+    {
+        public static readonly DependencyProperty DataGridDoubleClickProperty = 
+            DependencyProperty.RegisterAttached("DataGridDoubleClickCommand", typeof (ICommand), typeof (Commands), new PropertyMetadata(AttachOrRemoveDataGridDoubleClickEvent));
+
+        public static ICommand GetDataGridDoubleClickCommand(DependencyObject obj)
+        {
+            return (ICommand) obj.GetValue(DataGridDoubleClickProperty);
+        }
+
+        public static void SetDataGridDoubleClickCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(DataGridDoubleClickProperty, value);
+        }
+
+        public static void AttachOrRemoveDataGridDoubleClickEvent(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            var dataGrid = obj as DataGrid;
+            if (dataGrid == null) return;
+
+            if (args.OldValue == null && args.NewValue != null)
+            {
+                dataGrid.MouseDoubleClick += ExecuteDataGridDoubleClick;
+            }
+            else if (args.OldValue != null && args.NewValue == null)
+            {
+                dataGrid.MouseDoubleClick -= ExecuteDataGridDoubleClick;
+            }
+        }
+
+        private static void ExecuteDataGridDoubleClick(object sender, MouseButtonEventArgs args)
+        {
+            var dependencyObject = sender as DependencyObject;
+            if (dependencyObject == null) return;
+
+            var command = (ICommand) dependencyObject.GetValue(DataGridDoubleClickProperty);
+            if (command == null) return;
+
+            if (command.CanExecute(dependencyObject)) command.Execute(dependencyObject);
+        }
+    }
+}
