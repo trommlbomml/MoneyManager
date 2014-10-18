4a94f847cccdef52f8becb4e5661b9f4abd2b549
trommlbomml <peter_friedland@gmx.de>
2014-10-18 17:54:26 +0200

    1) TabControl Item Headers have same size measured from largest element.
    2) Styled AccountManagement Page further.

---
+++ MoneyManagerApplication/MoneyManagerApplication/Converters/UnifyTabWidthConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MoneyManagerApplication.Converters
+{
+    class UnifyTabWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            var tabControl = values[0] as TabControl;
+            if (tabControl == null) return DependencyProperty.UnsetValue;
+
+            var maxWidth = double.MinValue;
+            foreach (TabItem tabItem in tabControl.Items)
+            {
+                maxWidth = Math.Max(maxWidth, tabItem.ActualWidth);
+            }
+
+            return maxWidth;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
