414a8a4ddd2a799cbc12c69620f2247dcd64ebf0
trommlbomml <peter_friedland@gmx.de>
2015-06-28 20:05:17 +0200

    Improved View for Standing Orders

---
+++ MoneyManagerApplication/MoneyManagerApplication/Converters/BoolToHiddenConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MoneyManagerApplication.Converters
+{
+    class BoolToHiddenConverter : IValueConverter
+    {
+        public static readonly BoolToHiddenConverter Instance = new BoolToHiddenConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool) value ? Visibility.Visible : Visibility.Hidden;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class BoolToInvHiddenConverter : IValueConverter
+    {
+        public static readonly BoolToInvHiddenConverter Instance = new BoolToInvHiddenConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? Visibility.Hidden : Visibility.Visible;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
